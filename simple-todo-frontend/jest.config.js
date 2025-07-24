0ab6e88bc8a32993409024fe44795ff4b51764df
Mansi <manasivd030@gmail.com>
2025-07-24 13:55:34 +0200

    Minor changes in frontend files

--- simple-todo-frontend/jest.config.js
+++ simple-todo-frontend/jest.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  roots: ['<rootDir>/simple-todo-frontend/src'], // Ensure Jest is looking at the src folder
   transform: {
     '^.+\\.js$': 'babel-jest', // Use babel-jest to transform JS files
   },
+  testEnvironment: 'jsdom', // Use jsdom for testing React components
   transformIgnorePatterns: [
-    "/node_modules/(?!axios)"  // Ensure axios is transformed by Babel
+    '/node_modules/(?!axios)/', // Transform axios
   ],
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios'), // Ensure axios is properly resolved
+  },
 };
