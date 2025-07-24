0f9923b8806847417a6cb771e02f0621e2114cd7
Mansi <manasivd030@gmail.com>
2025-07-24 16:27:36 +0200

    Minor changes in frontend files

--- simple-todo-frontend/babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: [
-    '@babel/preset-env',    // Transpile modern JavaScript to older versions
-    '@babel/preset-react',  // Transpile JSX to regular JavaScript
-  ],
-};
